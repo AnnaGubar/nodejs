c90a6767378ab780e178f6c602dc38f725e29766
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-07-30 14:09:20 +0300

    d

--- README.md
+++ README.md
@@ -6,4 +6,18 @@
 - `npm lint:fix` &mdash; та ж перевірка лінтера, але з автоматичними виправленнями простих помилок
 
 ### Файлы
+`models/books.json` - данные
+`models/index.js` - методы обработки данных
+`app.js` - создание веб-сервера и работа с ним
+`routes/api/books.js` - обработчики адресов запросов
 `server.js` - запуск сервера
+`helpers/createError.js` - 
+`helpers/index.js` - 
+
+`next(error);` - озн что обработчик будет искать следующий .use() где обрабатывается error, у нас это выполняется в файле app.js
+```
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+```
--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express"); 
+const logger = require("morgan");
+const cors = require("cors");
 
+// ✅ express 1 - создание веб-сервера
+const app = express();
+
+// ✅ express 2 - создание обработчиков адресов
 const booksRouter = require("./routes/api/books");
 
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-const app = express()
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-
+// ✅ express 3 - привязываем обработчики к веб-серверу
 app.use("/api/books", booksRouter);
 
-
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  const {status = 500, message = "Server error"} = err;
-  res.status(status).json({ message })
-})
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
-module.exports = app
+// ✅ express 4 - в server.js запускаем веб-сервер
+module.exports = app;
